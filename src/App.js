c7477d4363ba9444fc31c51699eaf2755e5a3e97
Revanth <irevanth54@gmail.com>
2022-08-08 10:30:09 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <div className="App">
       <h1>raghu</h1>
     <h1> HAPPY BIRTHDAY RAGHU</h1>
-
-
-
     <h1>well come to hyd</h1>
 
     <h1 id="venki"> HAPPY BIRTHDAY VENKATESH</h1>
