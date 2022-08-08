2fc7b65ebebd929617270b989cab840131debf07
Revanth <irevanth54@gmail.com>
2022-08-08 10:16:43 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>raghu</h1>
     <h1> HAPPY BIRTHDAY RAGHU</h1>
-    <h1> HAPPY BIRTHDAY VENKATESH</h1>
+    <h1 id="venki"> HAPPY BIRTHDAY VENKATESH</h1>
     </div>
   );
 }
